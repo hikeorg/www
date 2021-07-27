b6a21bafcec445b1ae2852435535fa2ee1383124
Ricardo Casares <ricardocasares@gmail.com>
2021-07-27 23:07:47 +0200

    fix: remove pagination

--- theme.config.js
+++ theme.config.js
@@ -1,26 +1,34 @@
-import { Logo } from './components/Logo';
+import { Logo } from "./components/Logo";
 
 export default {
-  repository: 'https://github.com/hikeorg/hike-www', // project repo
-  docsRepository: 'https://github.com/hikeorg/hike-www', // docs repo
-  branch: 'master', // branch of docs
-  path: '/', // path of docs
-  titleSuffix: ' – Hike.sh',
-  nextLinks: true,
-  prevLinks: true,
+  repository: "https://github.com/hikeorg/hike-www", // project repo
+  docsRepository: "https://github.com/hikeorg/hike-www", // docs repo
+  branch: "master", // branch of docs
+  path: "/", // path of docs
+  titleSuffix: " – Hike.sh",
+  nextLinks: false,
+  prevLinks: false,
   search: true,
   customSearch: null, // customizable, you can use algolia for example
   darkMode: true,
   footer: true,
-  footerText: <><Logo /> Hike</>,
-  footerEditOnGitHubLink: true, // will link to the docs repo
-  logo: <>
-    <Logo /> Hike
-  </>,
-  head: <>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta name="description" content="Hike: GitHub Automation Apps" />
-    <meta name="og:title" content="Hike: GitHub Automation Apps" />
-    <link rel="icon" href="/hike.svg" />
-  </>
+  footerText: (
+    <>
+      <Logo /> Hike
+    </>
+  ),
+  footerEditOnGitHubLink: false, // will link to the docs repo
+  logo: (
+    <>
+      <Logo /> Hike
+    </>
+  ),
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="description" content="Hike: GitHub Automation Apps" />
+      <meta name="og:title" content="Hike: GitHub Automation Apps" />
+      <link rel="icon" href="/hike.svg" />
+    </>
+  ),
 };
